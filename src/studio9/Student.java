a9a6e7c66dda27b498eeca583c0f98be46b36e57
Doug Shook <dsshook@gmail.com>
2019-11-12 20:21:19 -0600

    studio 9 starter

---
+++ src/studio9/Student.java
@@ -0,0 +1,6 @@
+package studio9;
+
+public class Student {
+
+    //TODO: Copy your student class from lab 7 into this file
+}
---
+++ src/studio9/UniversityDatabase.java
@@ -0,0 +1,11 @@
+package studio9;
+
+public class UniversityDatabase {
+    //TODO: Complete this class according to the studio instructions
+
+
+    //TODO: Stub is included for you, complete according to studio instructions
+    public String lookupFullName(String accountName) {
+        return null;
+    }
+}
